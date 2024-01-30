015cd79bc3af5cf37974608fe3fcd208c27bd695
Dante <dante0im@gmail.com>
2024-01-30 18:12:44 -0300

    Implemented Skeleton

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import SkeletonList from "@/components/skeleton-list";
+import { Suspense } from "react";
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<Suspense
+			fallback={
+				<main className="flex gap-4 overflow-x-auto pt-1 px-1 pb-4">
+					<SkeletonList tasks={6} />
+					<SkeletonList tasks={5} />
+					<SkeletonList tasks={4} />
+				</main>
+			}
+		>
+			{children}
+		</Suspense>
+	);
+}
