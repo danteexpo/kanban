f3287c44b9ddae2458cbbec996383103314ae1ae
Dante <dante0im@gmail.com>
2024-01-31 16:45:36 -0300

    Changed prisma schema, added sign out and fetching of lists

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,29 @@
 import { prisma } from "@/lib/prisma";
 import Lists from "./_components/lists";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+	const { userId } = auth();
+
+	if (!userId) {
+		redirect("/sign-in");
+	}
+
 	const lists = await prisma.list.findMany({
-		include: { tasks: true },
+		where: {
+			authorId: userId,
+		},
+		include: {
+			tasks: {
+				orderBy: {
+					order: "asc",
+				},
+			},
+		},
+		orderBy: {
+			order: "asc",
+		},
 	});
 
 	return <Lists lists={lists} />;
--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import React from "react";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+import { useClerk } from "@clerk/clerk-react";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+	const { user, signOut } = useClerk();
+	const router = useRouter();
+
 	return (
 		<header className="flex items-center justify-between py-6">
 			<h1 className="text-3xl font-bold">Kanban</h1>
 			<div className="flex gap-2 items-center">
 				<ModeToggle />
+				{user && (
+					<Button
+						variant="outline"
+						onClick={() => signOut(() => router.push("/sign-in"))}
+					>
+						Sign out
+					</Button>
+				)}
 			</div>
 		</header>
 	);
